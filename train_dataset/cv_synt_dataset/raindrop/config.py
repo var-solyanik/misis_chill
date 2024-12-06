35e33973b796d56d5d3d01941b81f31b5931c7b9
Solyanik Varvara <solyanikvarvara@MacBook-Air-Solyanik.local>
2024-12-06 22:23:36 +0300

    kaif

---
+++ train_dataset/cv_synt_dataset/raindrop/config.py
@@ -0,0 +1,24 @@
+"""
+Arguments:
+maxR -- maximum drop radius
+minR -- minimum drop radius
+maxDrops -- maximum number of drops in the image
+minDrops -- minimum number of drops in the image
+edge_darkratio -- brightness reduction factor for drops edges
+return_label -- flag defining whether a label will be returned or just an image with generated raindrops
+A, B, C, D -- 4 tuples defining coordinates of Bezier control points in Alpha Map in drop radius scale
+"""
+
+cfg = {
+	'maxR': 150, # max not more then 150
+	'minR': 70,
+	'maxDrops': 20,
+	'minDrops': 4,
+	'edge_darkratio': 0.6,
+	'return_label': True,
+	'label_thres': 128,
+	'A': (1, 4.5),
+	'B': (3, 1),
+	'C': (1, 3),
+	'D': (3, 3)
+}
